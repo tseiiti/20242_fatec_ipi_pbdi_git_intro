d41abda282a495ca963b377cedf6a1f522a515ca
Oscar Taniguchi <tseiiti@gmail.com>
2024-09-10 13:35:17 +0000

    feat: funcao e teste somar, subtrair, multiplicar implementado

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+def somar(a, b):
+  return a + b
+
+def subtrair(a, b):
+  return somar(a, b * -1)
+
+def multiplicar(a, b):
+  return a * b
---
+++ teste_calculadora.py
@@ -0,0 +1,5 @@
+import calculadora
+
+print (calculadora.somar(1, 2))
+print(calculadora.subtrair(3, 2))
+print(calculadora.multiplicar(3, 4))
